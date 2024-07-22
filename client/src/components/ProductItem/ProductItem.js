9da4383b22830accec34975682b7fcc3d10589cc
Tharanga Anuradha <tharanga123anuradha@gmail.com>
2024-07-22 20:24:33 +0530

    product item redesign

--- client/src/components/ProductItem/ProductItem.js
+++ client/src/components/ProductItem/ProductItem.js
@@ -1,36 +1,29 @@
 import React from "react";
-import "./Styles/ProductItem.css";
 import { Link } from "react-router-dom";
+import "./Styles/ProductItem.css";
 
 function ProductItem({ img, categoryName, categoryColor, gender, item }) {
   return (
     <div className="col-lg-4 col-md-6 mb-4">
-      <div className="bg-image hover-zoom ripple">
+      <div className="product-item">
         <Link
           to={
             item
               ? `/collection/${item.product_id}`
               : `/collection/${gender}/${categoryName}`
           }
+          className="product-link"
         >
-          <img src={img} className="w-100" alt="Product" />
-          <div className="hover-overlay">
-            <div className="mask"></div>
-            <div className="text-overlay" style={{ color: categoryColor }}>
+          <img src={img} className="product-img w-100" alt="Product" />
+          <div className="overlay">
+            <div className="overlay-content" style={{ color: categoryColor }}>
               {item ? item.product_name : categoryName}
             </div>
-            <div
-              className="mask"
-              style={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
-            >
-              <div className="d-flex justify-content-start align-items-start h-100">
-                <h5>
-                  <span className="badge bg-body-tertiary pt-2 ms-3 mt-3 text-dark">
-                    {item && item.price + "$"}
-                  </span>
-                </h5>
+            {item && (
+              <div className="price-badge">
+                <span>{item.price}$</span>
               </div>
-            </div>
+            )}
           </div>
         </Link>
       </div>
