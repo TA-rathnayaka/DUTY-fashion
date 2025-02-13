ff5a76d2c0c36edfa4398bbdae7ba9b8fe1757cd
Tharanga Anuradha <104713256+TA-rathnayaka@users.noreply.github.com>
2025-02-13 08:44:14 +0530

    connect to database and fix packaga.json

--- server/database/db.js
+++ server/database/db.js
@@ -1,115 +1,75 @@
 import pg from "pg";
+import dotenv from "dotenv";
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "StyleStoreDB",
-  password: "password",
-  port: 5432,
-});
+dotenv.config();
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.POSTGRES_PASSWORD}@${process.env.RAILWAY_TCP_PROXY_DOMAIN}:${process.env.RAILWAY_TCP_PROXY_PORT}/${process.env.PGDATABASE}`;
 
-async function createDatabaseAndTables() {
-  try {
-    await db.connect();
+const db = new pg.Client(connectionString);
 
-    const dbCheckQuery = `SELECT 1 FROM pg_database WHERE datname = 'StyleStoreDB'`;
-    const dbRes = await db.query(dbCheckQuery);
+db.connect();
 
-    if (dbRes.rows.length === 0) {
-      await db.query(`CREATE DATABASE StyleStoreDB`);
-      console.log("Database StyleStoreDB created.");
-    } else {
-      console.log("Database StyleStoreDB already exists.");
-    }
+// const createTables = async () => {
+//   try {
+//     await db.connect();
 
-    db.end();
-    const newDbClient = new pg.Client({
-      user: "postgres",
-      host: "localhost",
-      database: "StyleStoreDB",
-      password: "password",
-      port: 5432,
-    });
+//     const queries = [
+//       `CREATE TABLE IF NOT EXISTS user_table (
+//         user_id SERIAL PRIMARY KEY,
+//         first_name VARCHAR(250) NOT NULL,
+//         last_name VARCHAR(250),
+//         email VARCHAR(250) NOT NULL UNIQUE,
+//         password VARCHAR(250) NOT NULL
+//       );`,
 
-    await newDbClient.connect();
+//       `CREATE TABLE IF NOT EXISTS admin_table (
+//         admin_id SERIAL PRIMARY KEY,
+//         user_id INTEGER REFERENCES user_table(user_id) UNIQUE,
+//         admin_role VARCHAR(250) NOT NULL
+//       );`,
 
-    const tableCheckQueries = [
-      `SELECT to_regclass('public.user_table')`,
-      `SELECT to_regclass('public.admin_table')`,
-      `SELECT to_regclass('public.product_table')`,
-      `SELECT to_regclass('public.item_table')`,
-      `SELECT to_regclass('public.user_product')`,
-      `SELECT to_regclass('public.product_image')`,
-    ];
+//       `CREATE TABLE IF NOT EXISTS product_table (
+//         product_id SERIAL PRIMARY KEY,
+//         product_name VARCHAR(250) UNIQUE NOT NULL,
+//         description TEXT NOT NULL,
+//         gender VARCHAR(10) NOT NULL,
+//         category VARCHAR(250) NOT NULL
+//       );`,
 
-    const tableChecks = await Promise.all(
-      tableCheckQueries.map((query) => newDbClient.query(query))
-    );
+//       `CREATE TABLE IF NOT EXISTS item_table (
+//         item_id SERIAL PRIMARY KEY,
+//         product_id INTEGER REFERENCES product_table(product_id),
+//         size VARCHAR(10) NOT NULL,
+//         amount INTEGER NOT NULL,
+//         price DECIMAL NOT NULL
+//       );`,
 
-    const tablesExist = tableChecks.every(
-      (res) => res.rows[0].to_regclass !== null
-    );
+//       `CREATE TABLE IF NOT EXISTS user_product (
+//         user_id INTEGER REFERENCES user_table(user_id),
+//         item_id INTEGER REFERENCES item_table(item_id),
+//         wanted_amount INTEGER NOT NULL,
+//         PRIMARY KEY(user_id, item_id)
+//       );`,
 
-    if (tablesExist) {
-      console.log("All tables already exist.");
-    } else {
-      const createTablesQuery = `
-        CREATE TABLE IF NOT EXISTS user_table (
-          user_id SERIAL PRIMARY KEY,
-          first_name VARCHAR(250) NOT NULL,
-          last_name VARCHAR(250),
-          email VARCHAR(250) NOT NULL UNIQUE,
-          password VARCHAR(250) NOT NULL
-        );
+//       `CREATE TABLE IF NOT EXISTS product_image (
+//         image_id SERIAL PRIMARY KEY,
+//         product_id INTEGER REFERENCES product_table(product_id),
+//         image_url TEXT NOT NULL,
+//         is_primary BOOLEAN DEFAULT FALSE
+//       );`,
+//     ];
 
-        CREATE TABLE IF NOT EXISTS admin_table (
-          admin_id SERIAL PRIMARY KEY,
-          user_id INTEGER REFERENCES user_table(user_id) UNIQUE,
-          admin_role VARCHAR(250) NOT NULL
-        );
+//     for (const query of queries) {
+//       await db.query(query);
+//     }
 
-        CREATE TABLE IF NOT EXISTS product_table (
-          product_id SERIAL PRIMARY KEY,
-          product_name VARCHAR(250) UNIQUE NOT NULL,
-          description TEXT NOT NULL,
-          gender VARCHAR(10) NOT NULL,
-          category VARCHAR(250) NOT NULL
-        );
+//     console.log("All tables created successfully!");
+//   } catch (err) {
+//     console.error("Error creating tables:", err);
+//   } finally {
+//     await db.end();
+//   }
+// };
 
-        CREATE TABLE IF NOT EXISTS item_table (
-          item_id SERIAL PRIMARY KEY,
-          product_id INTEGER REFERENCES product_table(product_id),
-          size VARCHAR(10) NOT NULL,
-          amount INTEGER NOT NULL,
-          price DECIMAL NOT NULL
-        );
-
-        CREATE TABLE IF NOT EXISTS user_product (
-          user_id INTEGER REFERENCES user_table(user_id),
-          item_id INTEGER REFERENCES item_table(item_id),
-          wanted_amount INTEGER NOT NULL,
-          PRIMARY KEY(user_id, item_id)
-        );
-
-        CREATE TABLE IF NOT EXISTS product_image (
-          image_id SERIAL PRIMARY KEY,
-          product_id INTEGER REFERENCES product_table(product_id),
-          image_url TEXT NOT NULL,
-          is_primary BOOLEAN DEFAULT FALSE
-        );
-      `;
-
-      await newDbClient.query(createTablesQuery);
-      console.log("Tables created successfully!");
-    }
-
-    newDbClient.end();
-  } catch (error) {
-    console.error("Error creating database and tables:", error);
-  }
-}
-
-// Call the function to create database and tables if needed
-createDatabaseAndTables();
+// createTables();
 
 export default db;
