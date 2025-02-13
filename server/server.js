f09f41c5aa5bbf3fbe315140b3b5eca97da4665f
Tharanga Anuradha <104713256+TA-rathnayaka@users.noreply.github.com>
2025-02-13 19:45:16 +0530

    fix login issue

--- server/server.js
+++ server/server.js
@@ -22,7 +22,6 @@ app.use(
   })
 );
 
-app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
@@ -40,6 +39,7 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.static("public"));
 app.use(router);
 
 async function data() {
